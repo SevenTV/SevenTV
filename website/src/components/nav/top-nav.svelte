16dd869861c9b72e5a688c264b8eb44b6c2a0ca6
Lennart Kloock <lennart.kloock@protonmail.com>
2024-01-26 18:25:27 +0100

    feat(website): add top nav

---
+++ website/src/components/nav/top-nav.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import Logo from "$/components/icons/logo.svelte";
+    import Tabs from "./tabs.svelte";
+</script>
+
+<nav>
+    <div class="links">
+        <a class="home" href="/">
+            <Logo />
+        </a>
+        <Tabs
+            tabs={[
+                { name: "Emotes", pathname: "/emotes" },
+                { name: "Discover", pathname: "/discover" },
+                { name: "Store", pathname: "/store" },
+                { name: "More", pathname: "/more" },
+            ]}
+        />
+    </div>
+    <search>Search</search>
+    <div>User</div>
+</nav>
+
+<style lang="scss">
+    nav {
+        background-color: var(--bg-dark);
+        border-bottom: 1px solid var(--border);
+        padding: 0 2rem;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 2rem;
+    }
+
+    .links {
+        display: flex;
+        gap: 2rem;
+
+        .home {
+            display: flex;
+            align-items: center;
+        }
+    }
+</style>
---
+++ website/src/routes/+error.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    import Troll from "$/components/icons/troll.svelte";
+	import { page } from "$app/stores";
+
+	let title = $page.error?.message ?? "Error";
+</script>
+
+<svelte:head>
+	<title>7TV - {title}</title>
+</svelte:head>
+
+<div class="container">
+    <div class="troll">
+        <Troll />
+    </div>
+	<h1>{title}</h1>
+	<span>
+		{#if $page.status === 401}
+			You are unauthorized to view this page.
+		{:else if $page.status === 403}
+			You do not have permission to view this page.
+		{:else if $page.status === 404}
+			The page you were looking for could not be found.
+		{:else}
+			An internal error occurred.
+			<br />
+			You can try refreshing the page.
+			<br />
+			If the problem persists, please contact us.
+		{/if}
+	</span>
+	<a href="/">Go Back</a>
+</div>
+
+<style lang="scss">
+	.container {
+        height: 100%;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 1rem;
+		text-align: center;
+
+		font-size: 1.25rem;
+	}
+
+    .troll {
+        font-size: 12rem;
+    }
+
+	a {
+		color: var(--text);
+	}
+</style>
