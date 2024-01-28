d2adac21f3ab52b7b89b61aa633d21b0604443cf
Lennart Kloock <lennart.kloock@protonmail.com>
2024-01-28 22:52:49 +0100

    feat(website): mobile menu

--- website/src/routes/sign-in/+page.svelte
+++ website/src/routes/sign-in/+page.svelte
@@ -1,5 +1,11 @@
+<script lang="ts">
+    import { user } from "$/lib/stores";
+</script>
+
 <svelte:head>
     <title>Sign In - 7TV</title>
 </svelte:head>
 
 <h1>Sign In</h1>
+
+<button class="button primary" on:click={() => ($user = true)}>Fake login xdd</button>
---
+++ website/src/routes/tos/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+    <title>Terms of Service - 7TV</title>
+</svelte:head>
+
+<h1>Terms of Service</h1>
