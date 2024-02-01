9888050334363e550260a5c2455bac24d950c8a5
Lennart Kloock <lennart.kloock@protonmail.com>
2024-02-02 00:54:34 +0100

    fix(website): scuffed accessible select

--- website/src/components/select.svelte
+++ website/src/components/select.svelte
@@ -4,16 +4,13 @@
 	import mouseTrap from "$/lib/mouseTrap";
 	import { fade } from "svelte/transition";
 
-    type Option = { name: string; key: number; };
-
-    export let options: Option[];
-    export let selected: number = options[0].key;
-
-    $: selectedName = options.find((v) => v.key === selected)?.name;
+    export let options: string[];
+    export let selected: string | null = options[0] ?? null;
 
     let expanded = false;
 
     function toggle() {
+        console.log("toggle");
 		expanded = !expanded;
 	}
 
@@ -25,20 +22,28 @@
 <button
 	on:click={toggle}
 	use:mouseTrap={close}
-    class="button secondary active"
+    class="button secondary select"
     class:expanded={expanded}
+    tabindex="-1"
 >
-    {selectedName ?? "Select"}
+    <select bind:value={selected} on:click={toggle} on:keypress={toggle}>
+        {#each options as option}
+            <option value={option}>
+                {option}
+            </option>
+        {/each}
+    </select>
+    {selected ?? "Select"}
     <Fa icon={faChevronDown} fw />
     {#if expanded}
         <div class="dropped" transition:fade={{ duration: 100 }}>
             {#each options as option}
                 <button
                     class="button"
-                    class:secondary={selected === option.key}
-                    on:click={() => (selected = option.key)}
+                    class:secondary={selected === option}
+                    on:click={() => (selected = option)}
                 >
-                    {option.name}
+                    {option}
                 </button>
             {/each}
         </div>
@@ -46,13 +51,36 @@
 </button>
 
 <style lang="scss">
-    .button.active {
+    select {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        outline: none;
+        margin: 0;
+        padding: 0;
+        border: none;
+        width: 0;
+
+        display: inline;
+        clip: rect(0 0 0 0);
+        clip-path: inset(50%);
+        height: 1px;
+        overflow: hidden;
+        position: absolute;
+        white-space: nowrap;
+        width: 1px;
+    }
+
+    .button.select {
         position: relative;
         width: 100%;
         justify-content: space-between;
         border: transparent 1px solid;
-        border-bottom: none;
         background-color: var(--secondary);
+
+        &:focus-within {
+            border-color: var(--primary);
+        }
 
         &.expanded {
             border-color: var(--border);
