4c7ad26df0acf0aeee8ec148702ca592123463fb
Lennart Kloock <lennart.kloock@protonmail.com>
2024-02-10 22:36:51 +0100

    feat(website): emote pages

---
+++ website/src/components/emote-preview.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+    export let name = "emoteName";
+    export let index = 0;
+</script>
+
+<div class="emote">
+    <div class="image" style="animation-delay: {index * 10}ms"></div>
+    <span class="name">{name}</span>
+    <span class="user">username</span>
+</div>
+
+<style lang="scss">
+    .emote {
+        width: 10rem;
+        height: 10rem;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        background-color: var(--bg-medium);
+        border: 1px solid transparent;
+        border-radius: 0.25rem;
+        cursor: pointer;
+
+        &:hover, &:focus-visible {
+            border-color: var(--border);
+        }
+    }
+
+    @keyframes loading {
+        0% {
+            background-color: var(--secondary);
+        }
+        50% {
+            background-color: var(--secondary-active);
+        }
+        100% {
+            background-color: var(--secondary);
+        }
+    }
+
+    .image {
+        width: 5rem;
+        height: 5rem;
+        margin-bottom: 0.5rem;
+
+        background-color: var(--secondary);
+        animation: loading 1s infinite;
+    }
+
+    .name {
+        font-weight: 500;
+    }
+
+    .user {
+        font-size: 0.75rem;
+        font-weight: 500;
+    }
+</style>
---
+++ website/src/routes/emotes/(emotes)/+layout.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import Tabs from "$/components/tabs.svelte";
+	import { sideBar } from "$/lib/stores";
+	import { faArrowRightToLine, faFolder } from "@fortawesome/pro-regular-svg-icons";
+	import { faGrid, faGridHorizontal } from "@fortawesome/pro-solid-svg-icons";
+	import Fa from "svelte-fa";
+</script>
+
+<div class="nav-bar">
+    <div class="buttons">
+        {#if !$sideBar}
+            <button class="button expand" on:click={() => ($sideBar = true)}>
+                <Fa icon={faArrowRightToLine} size="1.2x" fw />
+            </button>
+        {/if}
+        <Tabs tabs={[
+            { name: "Top", pathname: "/emotes/top" },
+            { name: "Trending", pathname: "/emotes" },
+        ]} />
+    </div>
+    <div class="buttons">
+        <button class="button secondary icon-left hide-on-mobile">
+            <Fa icon={faFolder} fw />
+            Personal Emotes
+        </button>
+        <button class="button">
+            <Fa icon={faGridHorizontal} fw />
+        </button>
+        <button class="button">
+            <Fa icon={faGrid} fw />
+        </button>
+    </div>
+</div>
+<slot />
+
+<style lang="scss">
+	.nav-bar {
+		display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 1rem;
+	}
+
+    .buttons {
+        display: flex;
+        gap: 0.5rem;
+    }
+</style>
