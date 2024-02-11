1666fd164f1dd339436e25c056e5836a0e91dca6
Lennart Kloock <lennart.kloock@protonmail.com>
2024-02-11 03:50:55 +0100

    feat(website): add shop

---
+++ website/src/components/store/banner.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+	import {
+		faBadge,
+		faBrush,
+		faSmilePlus,
+		faTicket,
+		faUserCircle,
+	} from "@fortawesome/pro-regular-svg-icons";
+	import Fa from "svelte-fa";
+
+	export let title: string;
+	export let subtitle: string;
+</script>
+
+<section class="banner">
+	<div class="title">
+		<span>
+			<h1>{title}</h1>
+			<p>{subtitle}</p>
+		</span>
+		<button class="button no-bg hide-on-desktop">Learn more</button>
+	</div>
+	<div class="icons hide-on-mobile">
+		<Fa icon={faBrush} size="1.5x" />
+		<Fa icon={faUserCircle} size="1.5x" />
+		<Fa icon={faBadge} size="1.5x" />
+		<Fa icon={faTicket} size="1.5x" />
+		<Fa icon={faSmilePlus} size="1.5x" />
+	</div>
+</section>
+
+<style lang="scss">
+	.banner {
+		border-radius: 0.5rem;
+		background: radial-gradient(
+				100% 100% at 50% 0%,
+				#ddc4fd 0%,
+				#9227cf 33.33%,
+				#3f188b 64.58%,
+				#1f113e 80.73%,
+				#0e0c13 93.75%
+			),
+			var(--bg-medium);
+		border-radius: 0.5rem;
+
+		min-width: 12rem;
+		min-height: 11.25rem;
+		padding: 1.25rem 2.5rem;
+
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		.title {
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			gap: 1rem;
+
+			.button {
+				font-weight: 300;
+				border: 1px solid var(--text-light);
+			}
+		}
+
+		h1 {
+			font-size: 1.875rem;
+			font-weight: 700;
+		}
+
+		p {
+			font-weight: 300;
+		}
+
+		.icons {
+			padding: 0 2.75rem;
+
+			display: flex;
+			gap: 3.5rem;
+			flex-wrap: wrap;
+			align-items: center;
+			justify-content: center;
+		}
+	}
+
+	@media screen and (max-width: 768px) {
+		.banner {
+			padding: 1rem;
+		}
+	}
+</style>
---
+++ website/src/components/store/benefits.svelte
@@ -0,0 +1,168 @@
+<script lang="ts">
+	import {
+		faBadge,
+		faBrush,
+		faSendBackward,
+		faSmilePlus,
+		faTicket,
+		faUserCircle,
+	} from "@fortawesome/pro-regular-svg-icons";
+	import Fa from "svelte-fa";
+</script>
+
+<div>
+	<h2>Subscription Benefits</h2>
+	<div class="benefits">
+		<div class="benefit">
+			<div class="gradient g1">
+				<Fa icon={faUserCircle} size="3x" />
+			</div>
+			<h3>Animated Profile Picture</h3>
+		</div>
+		<div class="benefit">
+			<div class="gradient g2">
+				<Fa icon={faSmilePlus} size="3x" />
+			</div>
+			<h3>Personal Emotes</h3>
+		</div>
+		<div class="benefit">
+			<div class="gradient g3">
+				<Fa icon={faBrush} size="3x" />
+			</div>
+			<h3>Nametag Paints</h3>
+		</div>
+		<div class="benefit">
+			<div class="gradient g4">
+				<Fa icon={faBadge} size="3x" />
+			</div>
+			<h3>Subscriber Badge</h3>
+		</div>
+		<div class="benefit">
+			<div class="gradient g5">
+				<Fa icon={faSendBackward} size="3x" />
+			</div>
+			<h3>Zero-Width Emotes</h3>
+		</div>
+		<div class="benefit">
+			<div class="gradient g6">
+				<Fa icon={faTicket} size="3x" />
+			</div>
+			<h3>Raffle Ticket</h3>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+    h2 {
+		font-size: 1rem;
+		font-weight: 600;
+		margin-bottom: 1rem;
+	}
+
+	h3 {
+		font-size: 0.75rem;
+		font-weight: 500;
+	}
+
+	.benefits {
+		display: flex;
+		gap: 1rem;
+
+        overflow-x: auto;
+        max-width: calc(100vw - 1rem);
+        margin-right: -1rem;
+	}
+
+	.benefit {
+		background-color: var(--bg-medium);
+		border-radius: 0.5rem;
+
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+
+        cursor: pointer;
+
+        transition: background-color 0.2s;
+
+        &:hover, &:focus-visible {
+            background-color: var(--bg-light);
+        }
+
+		.gradient {
+			height: 8.5rem;
+			width: 10rem;
+
+			border-radius: 0.5rem;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			&.g1 {
+				background: radial-gradient(
+						150% 180% at 50% 180%,
+						#ff922f 0%,
+						#ffb800 33%,
+						#43006d 64%,
+						#0b0b0b 80%
+					),
+					var(--secondary);
+			}
+
+			&.g2 {
+				background: radial-gradient(
+						150% 180% at 50% 180%,
+						#fff 0%,
+						#ff3535 33%,
+						#43006d 64%,
+						#0b0b0b 80%
+					),
+					var(--secondary);
+			}
+
+			&.g3 {
+				background: radial-gradient(
+						150% 180% at 50% 180%,
+						#ff922f 0%,
+						#ff11bc 33%,
+						#43006d 64%,
+						#0b0b0b 80%
+					),
+					var(--secondary);
+			}
+
+			&.g4 {
+				background: radial-gradient(
+						150% 180% at 50% 180%,
+						#ff922f 0%,
+						#9611ff 33%,
+						#43006d 64%,
+						#0b0b0b 80%
+					),
+					var(--secondary);
+			}
+
+			&.g5 {
+				background: radial-gradient(
+						150% 180% at 50% 180%,
+						#ff922f 0%,
+						#11f1ff 33%,
+						#43006d 64%,
+						#0b0b0b 80%
+					),
+					var(--secondary);
+			}
+
+			&.g6 {
+				background: radial-gradient(
+						150% 180% at 50% 180%,
+						#ff922f 0%,
+						#11ff54 33%,
+						#43006d 64%,
+						#0b0b0b 80%
+					),
+					var(--secondary);
+			}
+		}
+	}
+</style>
