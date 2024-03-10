76cafb619fd00101ff9e502170e6ba7b9f67313a
Lennart Kloock <lennart.kloock@protonmail.com>
2024-03-10 17:39:25 +0100

    feat(website): delete emote set dialog mock

--- website/src/components/dialogs/edit-emote-set-dialog.svelte
+++ website/src/components/dialogs/edit-emote-set-dialog.svelte
@@ -4,10 +4,19 @@
 	import Checkbox from "../checkbox.svelte";
 	import Button from "../button.svelte";
 	import TextInput from "../input/text-input.svelte";
+	import DeleteEmoteSetDialog from "./delete-emote-set-dialog.svelte";
 
 	export let mode: DialogMode = DialogMode.Hidden;
+
+	let deleteDialogMode = DialogMode.Hidden;
+
+	function onDeleteClick() {
+		mode = DialogMode.Hidden;
+		deleteDialogMode = DialogMode.Shown;
+	}
 </script>
 
+<DeleteEmoteSetDialog bind:mode={deleteDialogMode} />
 <Dialog bind:mode>
 	<div class="layout">
 		<h1>Edit Emote Set</h1>
@@ -29,7 +38,8 @@
 		</div>
 	</div>
 	<div class="buttons">
-		<Button style="color: var(--error); margin-right: auto;">Delete</Button>
+		<Button style="color: var(--error); margin-right: auto;" on:click={onDeleteClick}>Delete</Button
+		>
 		<Button secondary on:click={() => (mode = DialogMode.Hidden)}>Cancel</Button>
 		<Button primary>Save</Button>
 	</div>
