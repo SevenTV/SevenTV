9bad903cd46dfb89767c9a71efcb23fb12e00d37
Lennart Kloock <lennart.kloock@protonmail.com>
2024-04-12 17:26:08 +0200

    feat: activities to clickhouse

--- database/activity.sql
+++ database/activity.sql
@@ -1,56 +1,49 @@
-CREATE TYPE emote_activity_kind AS ENUM (
-    'UPLOAD',
-    'EDIT',
-    'MERGE',
-    'DELETE'
-);
+-- Clickhouse SQL
 
 CREATE TABLE emote_activities (
-    "id" uuid PRIMARY KEY,
-    "emote_id" uuid NOT NULL, -- Ref: emotes.id -> On Delete Cascade
-    "actor_id" uuid, -- Ref: users.id -> On Delete Set Null
-    "kind" emote_activity_kind NOT NULL,
-    "data" jsonb NOT NULL DEFAULT '{}'
-);
-
-CREATE INDEX emote_activities_emote_id_index ON emote_activities ("emote_id");
-CREATE INDEX emote_activities_actor_id_index ON emote_activities ("actor_id");
-
-CREATE TYPE emote_set_activity_kind AS ENUM (
-    'CREATE',
-    'EDIT',
-    'DELETE'
-);
+    "emote_id" UUID,
+    "actor_id" UUID,
+    "kind" Enum(
+        'UPLOAD' = 0,
+        'EDIT' = 1,
+        'MERGE' = 2,
+        'DELETE' = 3
+    ),
+    "timestamp" DateTime DEFAULT NOW()
+)
+ENGINE = MergeTree
+PRIMARY KEY ("emote_id", "actor_id")
+ORDER BY ("emote_id", "actor_id", "timestamp");
 
 CREATE TABLE emote_set_activities (
-    "id" uuid PRIMARY KEY,
-    "emote_set_id" uuid NOT NULL, -- Ref: emote_sets.id -> On Delete Cascade
-    "actor_id" uuid, -- Ref: users.id -> On Delete Set Null
-    "kind" emote_set_activity_kind NOT NULL,
-    "data" jsonb NOT NULL DEFAULT '{}'
-);
-
-CREATE INDEX emote_set_activities_emote_set_id_index ON emote_set_activities ("emote_set_id");
-CREATE INDEX emote_set_activities_actor_id_index ON emote_set_activities ("actor_id");
-
-CREATE TYPE user_activity_kind AS ENUM (
-    'LOGIN',
-    'LOGOUT',
-    'REGISTER',
-    'EDIT',
-    'MERGE',
-    'DELETE',
-    'BAN',
-    'UNBAN'
-);
+    "emote_set_id" UUID,
+    "actor_id" UUID,
+    "kind" Enum(
+        'CREATE' = 0,
+        'EDIT' = 1,
+        'DELETE' = 2
+    ),
+    "timestamp" DateTime DEFAULT NOW()
+)
+ENGINE = MergeTree
+PRIMARY KEY ("emote_set_id", "actor_id")
+ORDER BY ("emote_set_id", "actor_id", "timestamp");
 
 CREATE TABLE user_activities (
-    "id" uuid PRIMARY KEY,
-    "user_id" uuid NOT NULL, -- Ref: users.id -> On Delete Cascade
-    "actor_id" uuid, -- Ref: users.id -> On Delete Set Null
-    "kind" user_activity_kind NOT NULL,
-    "data" jsonb NOT NULL DEFAULT '{}'
-);
-
-CREATE INDEX user_activities_user_id_index ON user_activities ("user_id");
-CREATE INDEX user_activities_actor_id_index ON user_activities ("actor_id");
+    "user_id" UUID,
+    "actor_id" UUID,
+    "kind" Enum(
+        'REGISTER' = 0,
+        'LOGIN' = 1,
+        'LOGOUT' = 2,
+        'EDIT' = 3,
+        'DELETE' = 4,
+        'MERGE' = 5,
+        'BAN' = 6,
+        'UNBAN' = 7
+    ),
+    "timestamp" DateTime DEFAULT NOW()
+)
+ENGINE = MergeTree
+PRIMARY KEY ("user_id", "actor_id")
+ORDER BY ("user_id", "actor_id", "timestamp");
