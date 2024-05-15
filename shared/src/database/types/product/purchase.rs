4dbd069bdfbdec26b2524a07ccd29e9fdd8ed654
Lennart Kloock <lennart.kloock@protonmail.com>
2024-05-15 18:18:41 +0200

    feat: payment model

--- shared/src/database/types/product/purchase.rs
+++ shared/src/database/types/product/purchase.rs
@@ -1,174 +1,132 @@
+use mongodb::bson::DateTime;
+use stripe::ProductId;
+
 use crate::database::{Collection, Id, UserId};
 
-use super::{GatewayProvider, PriceId, RedeemCodeId};
+use super::PriceId;
+
+pub type InvoiceId = Id<Invoice>;
+
+/// The Invoice object represents an invoice made for a user
+/// All disputes and refunds are done manually by an admin. An admin will determin how to effect the `Purchase` object on a case by case basis. (e.g. strip the user of the item or not)
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct Invoice {
+	#[serde(rename = "_id")]
+	pub id: InvoiceId,
+	/// The person who the invoice is for
+	pub user_id: UserId,
+	pub state: InvoiceState,
+	pub purchases_id: Vec<PurchaseId>,
+	// Additional data about the payment (e.g. amount, currency, stripe payment id, etc)
+	// ...
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub enum InvoiceState {
+	Draft,
+	Finalized,
+	Failed,
+	Successful,
+	Refunded {
+		// Additional data about the refund
+		// ...
+	},
+	Disputed {
+		// Additional data about the dispute
+		// ...
+	},
+}
+
+impl Collection for Invoice {
+	const COLLECTION_NAME: &'static str = "invoices";
+}
 
 pub type PurchaseId = Id<Purchase>;
 
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
-#[serde(deny_unknown_fields)]
+/// A purchase is an item that a user owns
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub struct Purchase {
 	#[serde(rename = "_id")]
 	pub id: PurchaseId,
-	pub purchase_kind: PurchaseKind,
+	/// The person who receives the purchase
+	pub user_id: UserId,
+	/// A purchase can either be a subscription or a product
+	/// Subscription is a product that requires renewal
+	/// Product is a one-time purchase which does not require renewal
 	pub data: PurchaseData,
 }
 
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
-#[serde(deny_unknown_fields, rename_all = "snake_case", tag = "kind", content = "data")]
-pub enum PurchaseKind {
-	Normal {
-		user_id: UserId,
-	},
-	Gift {
-		recipient: GiftRecipient,
-		buyer_id: UserId,
-		/// None if the gift has not been redeemed or accepted yet
-		user_id: Option<UserId>,
-	},
-}
-
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
-#[serde(deny_unknown_fields, rename_all = "snake_case", tag = "kind", content = "data")]
-pub enum GiftRecipient {
-	/// The gift can be redeemed by anyone
-	Anyone {
-		code: String,
-	},
-	/// The gift can only be redeemed by one of the specified users
-	User {
-		users: Vec<UserId>,
-	},
-}
-
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
-#[serde(deny_unknown_fields, rename_all = "snake_case", tag = "kind", content = "data")]
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub enum PurchaseData {
-	OneTime {
-		items: Vec<PriceId>,
-	},
-	RedeemCode {
-		id: RedeemCodeId,
-	},
 	Subscription {
-		provider: GatewayProvider,
-		/// old subscriptions are missing this field
-		provider_id: Option<String>,
-		status: SubscriptionStatus,
-		prices: Vec<PriceId>,
-		cancel_at_period_end: bool,
-		cancel_at: Option<mongodb::bson::DateTime>,
-		created: mongodb::bson::DateTime,
-		ended_at: Option<mongodb::bson::DateTime>,
-		trial_end: Option<mongodb::bson::DateTime>,
+		/// The id of the subscription at the provider (Stripe or Paypal)
+		/// Might be None for old subscriptions
+		subscription_provider_id: Option<String>,
+		/// Status of the subscription
+		recurring: Option<PurchaseRecurring>,
+		/// Allow a grace period for the subscription
+		allow_grace_period: bool,
+		/// The different periods of time the subscription is active for or is currently active for
+		periods: Vec<PurchaseSubscriptionPeriod>,
+		/// Future periods that are scheduled to be created
+		future_periods: Vec<PurchaseSubscriptionFuturePeriod>,
 	},
-	SubscriptionSchedule {
-		provider_id: stripe::SubscriptionScheduleId,
-		status: SubscriptionScheduleStatus,
-		subscription_id: stripe::SubscriptionId,
-		current_phase: SubscriptionScheduleCurrentPhase,
+	Product {
+		/// The product that was purchased
+		price_id: PriceId,
+		/// The payment that was made
+		invoice_id: InvoiceId,
 	},
 }
 
-#[derive(Copy, Clone, Debug, serde_repr::Deserialize_repr, serde_repr::Serialize_repr, Eq, PartialEq)]
-#[repr(u8)]
-pub enum SubscriptionStatus {
-	Active = 0,
-	Canceled = 1,
-	Incomplete = 2,
-	IncompleteExpired = 3,
-	PastDue = 4,
-	Paused = 5,
-	Trialing = 6,
-	Unpaid = 7,
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct PurchaseRecurring {
+	/// The date the subscription was last renewed
+	pub renewed_at: DateTime,
+	/// Next renewal date
+	pub next_renewal_at: DateTime,
+	/// The payment for the subscription failed
+	pub payment_failed: bool,
+	/// The subscription was cancelled
+	pub cancelled: bool,
 }
 
-impl From<stripe::SubscriptionStatus> for SubscriptionStatus {
-	fn from(value: stripe::SubscriptionStatus) -> Self {
-		match value {
-			stripe::SubscriptionStatus::Active => Self::Active,
-			stripe::SubscriptionStatus::Canceled => Self::Canceled,
-			stripe::SubscriptionStatus::Incomplete => Self::Incomplete,
-			stripe::SubscriptionStatus::IncompleteExpired => Self::IncompleteExpired,
-			stripe::SubscriptionStatus::PastDue => Self::PastDue,
-			stripe::SubscriptionStatus::Paused => Self::Paused,
-			stripe::SubscriptionStatus::Trialing => Self::Trialing,
-			stripe::SubscriptionStatus::Unpaid => Self::Unpaid,
-		}
-	}
+/// Each payment represents a period of time the subscription is active for
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct PurchaseSubscriptionPeriod {
+	pub price_id: PriceId,
+	/// This is the start time of the period, this is not the time the payment was made.
+	/// because if the payment was made late due to whatever reason, and the subscription was in a grace period
+	/// we wouls start this period from the end of the previous period to make sure no gap is created.
+	pub start: DateTime,
+	/// The time that this period ends. After this time the effect of the subscription is no longer active, unless there is a grace period.
+	/// If the subscription is in a grace period, the subscription is still active until the end of the grace period.
+	pub end: DateTime,
+	/// The payment that created this period.
+	pub created_by: PurchaseSubscriptionPeriodCreatedBy,
+	/// If this period is enabled or not, typically this is not enabled until the invoice is paid.
+	/// Or if the invoice is refunded.
+	pub enabled: bool,
+	// tier: SubscriptionTier,
 }
 
-impl From<SubscriptionStatus> for stripe::SubscriptionStatus {
-	fn from(value: SubscriptionStatus) -> Self {
-		match value {
-			SubscriptionStatus::Active => Self::Active,
-			SubscriptionStatus::Canceled => Self::Canceled,
-			SubscriptionStatus::Incomplete => Self::Incomplete,
-			SubscriptionStatus::IncompleteExpired => Self::IncompleteExpired,
-			SubscriptionStatus::PastDue => Self::PastDue,
-			SubscriptionStatus::Paused => Self::Paused,
-			SubscriptionStatus::Trialing => Self::Trialing,
-			SubscriptionStatus::Unpaid => Self::Unpaid,
-		}
-	}
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct PurchaseSubscriptionFuturePeriod {
+	/// The time that this period ends. After this time the effect of the subscription is no longer active, unless there is a grace period.
+	/// If the subscription is in a grace period, the subscription is still active until the end of the grace period.
+	pub end: Option<DateTime>,
+	/// The payment that created this period.
+	pub created_by: PurchaseSubscriptionPeriodCreatedBy,
 }
 
-#[derive(Copy, Clone, Debug, serde_repr::Deserialize_repr, serde_repr::Serialize_repr, Eq, PartialEq)]
-#[repr(u8)]
-pub enum SubscriptionScheduleStatus {
-	Active = 0,
-	Canceled = 1,
-	Completed = 2,
-	NotStarted = 3,
-	Released = 4,
-}
-
-impl From<stripe::SubscriptionScheduleStatus> for SubscriptionScheduleStatus {
-	fn from(value: stripe::SubscriptionScheduleStatus) -> Self {
-		match value {
-			stripe::SubscriptionScheduleStatus::Active => Self::Active,
-			stripe::SubscriptionScheduleStatus::Canceled => Self::Canceled,
-			stripe::SubscriptionScheduleStatus::Completed => Self::Completed,
-			stripe::SubscriptionScheduleStatus::NotStarted => Self::NotStarted,
-			stripe::SubscriptionScheduleStatus::Released => Self::Released,
-		}
-	}
-}
-
-impl From<SubscriptionScheduleStatus> for stripe::SubscriptionScheduleStatus {
-	fn from(value: SubscriptionScheduleStatus) -> Self {
-		match value {
-			SubscriptionScheduleStatus::Active => Self::Active,
-			SubscriptionScheduleStatus::Canceled => Self::Canceled,
-			SubscriptionScheduleStatus::Completed => Self::Completed,
-			SubscriptionScheduleStatus::NotStarted => Self::NotStarted,
-			SubscriptionScheduleStatus::Released => Self::Released,
-		}
-	}
-}
-
-#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
-#[serde(deny_unknown_fields)]
-pub struct SubscriptionScheduleCurrentPhase {
-	pub end_date: mongodb::bson::DateTime,
-	pub start_date: mongodb::bson::DateTime,
-}
-
-impl From<stripe::SubscriptionScheduleCurrentPhase> for SubscriptionScheduleCurrentPhase {
-	fn from(value: stripe::SubscriptionScheduleCurrentPhase) -> Self {
-		Self {
-			end_date: chrono::DateTime::from_timestamp(value.end_date, 0).unwrap().into(),
-			start_date: chrono::DateTime::from_timestamp(value.start_date, 0).unwrap().into(),
-		}
-	}
-}
-
-impl From<SubscriptionScheduleCurrentPhase> for stripe::SubscriptionScheduleCurrentPhase {
-	fn from(value: SubscriptionScheduleCurrentPhase) -> Self {
-		Self {
-			end_date: value.end_date.to_chrono().timestamp(),
-			start_date: value.start_date.to_chrono().timestamp(),
-		}
-	}
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub enum PurchaseSubscriptionPeriodCreatedBy {
+	/// The payment was made by the user
+	Invoice { invoice_id: InvoiceId },
+	/// The payment was made by a different user
+	Gift { user_id: UserId, invoice_id: InvoiceId },
+	/// This is a trial period that was created by the system
+	Trial {},
 }
 
 impl Collection for Purchase {
