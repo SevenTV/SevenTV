ca33c7a624274c7c6217ef619b549ca5daf30880
Lennart Kloock <lennart.kloock@protonmail.com>
2024-06-13 23:55:19 +0200

    feat(api): v3 roles

--- apps/api/src/http/v3/gql/mutations/roles.rs
+++ apps/api/src/http/v3/gql/mutations/roles.rs
@@ -1,7 +1,8 @@
 use std::sync::Arc;
 
 use async_graphql::{Context, InputObject, Object};
-use mongodb::bson::doc;
+use hyper::StatusCode;
+use mongodb::bson::{doc, to_bson};
 use mongodb::options::{FindOneAndUpdateOptions, ReturnDocument};
 use shared::database::{self, Collection, RolePermission};
 use shared::old_types::RoleObjectId;
@@ -79,13 +80,141 @@ impl RolesMutation {
 	}
 
 	#[graphql(guard = "PermissionGuard::one(RolePermission::Edit)")]
-	async fn edit_role(&self, role_id: RoleObjectId, data: EditRoleInput) -> Result<Role, ApiError> {
-		Err(ApiError::NOT_IMPLEMENTED)
+	async fn edit_role<'ctx>(
+		&self,
+		ctx: &Context<'ctx>,
+		role_id: RoleObjectId,
+		data: EditRoleInput,
+	) -> Result<Role, ApiError> {
+		let global: &Arc<Global> = ctx.data().map_err(|_| ApiError::INTERNAL_SERVER_ERROR)?;
+
+		let mut session = global.mongo().start_session(None).await.map_err(|err| {
+			tracing::error!(error = %err, "failed to start session");
+			ApiError::INTERNAL_SERVER_ERROR
+		})?;
+
+		session.start_transaction(None).await.map_err(|err| {
+			tracing::error!(error = %err, "failed to start transaction");
+			ApiError::INTERNAL_SERVER_ERROR
+		})?;
+
+		let mut role_ids_push = doc! {
+			"$each": [role_id.id()],
+		};
+
+		if let Some(position) = data.position {
+			role_ids_push.insert("$position", position);
+		}
+
+		let global_config = database::GlobalConfig::collection(global.db())
+			.find_one_and_update_with_session(
+				doc! {},
+				vec![
+					doc! {
+						"$pull": {
+							"role_ids": role_id.id(),
+						}
+					},
+					doc! {
+						"$push": {
+							"role_ids": role_ids_push,
+						}
+					},
+				],
+				FindOneAndUpdateOptions::builder()
+					.return_document(ReturnDocument::After)
+					.build(),
+				&mut session,
+			)
+			.await
+			.map_err(|e| {
+				tracing::error!(error = %e, "failed to update global config");
+				ApiError::INTERNAL_SERVER_ERROR
+			})?
+			.ok_or(ApiError::INTERNAL_SERVER_ERROR)?;
+
+		let mut update = doc! {};
+
+		if let Some(name) = data.name {
+			update.insert("name", name);
+		}
+
+		if let Some(color) = data.color {
+			update.insert("color", color);
+		}
+
+		match (data.allowed, data.denied) {
+			(Some(allowed), Some(denied)) => {
+				let allowed: u64 = allowed.parse().map_err(|_| ApiError::BAD_REQUEST)?;
+				let allowed = shared::old_types::RolePermission::from(allowed);
+				let denied: u64 = denied.parse().map_err(|_| ApiError::BAD_REQUEST)?;
+				let denied = shared::old_types::RolePermission::from(denied);
+
+				update.insert(
+					"permissions",
+					to_bson(&shared::old_types::RolePermission::to_new_permissions(allowed, denied))
+						.map_err(|_| ApiError::INTERNAL_SERVER_ERROR)?,
+				);
+			}
+			(None, None) => {}
+			_ => {
+				return Err(ApiError::new_const(
+					StatusCode::BAD_REQUEST,
+					"must provide both allowed and denied permissions",
+				));
+			}
+		}
+
+		let role = database::Role::collection(global.db())
+			.find_one_and_update_with_session(
+				doc! {
+					"_id": role_id.id(),
+				},
+				doc! {
+					"$set": update,
+				},
+				FindOneAndUpdateOptions::builder()
+					.return_document(ReturnDocument::After)
+					.build(),
+				&mut session,
+			)
+			.await
+			.map_err(|e| {
+				tracing::error!(error = %e, "failed to update role");
+				ApiError::INTERNAL_SERVER_ERROR
+			})?
+			.ok_or(ApiError::INTERNAL_SERVER_ERROR)?;
+
+		session.commit_transaction().await.map_err(|err| {
+			tracing::error!(error = %err, "failed to commit transaction");
+			ApiError::INTERNAL_SERVER_ERROR
+		})?;
+
+		Ok(Role::from_db(role, &global_config))
 	}
 
 	#[graphql(guard = "PermissionGuard::one(RolePermission::Delete)")]
-	async fn delete_role(&self, role_id: RoleObjectId) -> Result<String, ApiError> {
-		Err(ApiError::NOT_IMPLEMENTED)
+	async fn delete_role<'ctx>(&self, ctx: &Context<'ctx>, role_id: RoleObjectId) -> Result<String, ApiError> {
+		let global: &Arc<Global> = ctx.data().map_err(|_| ApiError::INTERNAL_SERVER_ERROR)?;
+
+		let res = database::Role::collection(global.db())
+			.delete_one(
+				doc! {
+					"_id": role_id.id(),
+				},
+				None,
+			)
+			.await
+			.map_err(|e| {
+				tracing::error!(error = %e, "failed to delete role");
+				ApiError::INTERNAL_SERVER_ERROR
+			})?;
+
+		if res.deleted_count == 1 {
+			Ok(String::new())
+		} else {
+			Err(ApiError::NOT_FOUND)
+		}
 	}
 }
 
