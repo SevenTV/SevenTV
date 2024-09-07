eeb16decaa4588de2d338e64612f7338b07c80ca
Lennart Kloock <lennart.kloock@protonmail.com>
2024-09-07 14:43:22 +0200

    fix(api): login

--- apps/api/src/http/v3/rest/auth/login.rs
+++ apps/api/src/http/v3/rest/auth/login.rs
@@ -1,13 +1,14 @@
 use std::sync::Arc;
 
 use hyper::StatusCode;
+use mongodb::options::UpdateOptions;
 use shared::database::queries::{filter, update};
 use shared::database::role::permissions::{PermissionsExt, UserPermission};
 use shared::database::stored_event::StoredEventUserSessionData;
 use shared::database::user::connection::{Platform, UserConnection};
 use shared::database::user::session::UserSession;
 use shared::database::user::{User, UserId};
-use shared::event::{InternalEvent, InternalEventData, InternalEventUserData};
+use shared::event::{InternalEvent, InternalEventData};
 
 use super::LoginRequest;
 use crate::connections;
@@ -30,6 +31,7 @@ const GOOGLE_AUTH_URL: &str =
 	"https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&include_granted_scopes=true&";
 const GOOGLE_AUTH_SCOPE: &str = "https://www.googleapis.com/auth/youtube.readonly";
 
+/// https://gist.github.com/lennartkloock/412323105bc913c7064664dc4f1568cb
 pub async fn handle_callback(global: &Arc<Global>, query: LoginRequest, cookies: &Cookies) -> Result<String, ApiError> {
 	let code = query
 		.code
@@ -64,9 +66,9 @@ pub async fn handle_callback(global: &Arc<Global>, query: LoginRequest, cookies:
 	// query user data from platform
 	let user_data = connections::get_user_data(global, platform, &token.access_token).await?;
 
-	let res = with_transaction(global, |mut tx| async move {
-		let mut user = tx
-			.find_one_and_update(
+	let user_id = with_transaction(global, |mut tx| async move {
+		let user = tx
+			.find_one(
 				filter::filter! {
 					User {
 						#[query(elem_match)]
@@ -76,42 +78,31 @@ pub async fn handle_callback(global: &Arc<Global>, query: LoginRequest, cookies:
 						}
 					}
 				},
-				update::update! {
-					#[query(set)]
-					User {
-						#[query(flatten, index = "$")]
-						connections: UserConnection {
-							platform_username: &user_data.username,
-							platform_display_name: &user_data.display_name,
-							platform_avatar_url: &user_data.avatar,
-							updated_at: chrono::Utc::now(),
-						},
-						updated_at: chrono::Utc::now(),
-					}
-				},
 				None,
 			)
 			.await?;
 
-		match (&user, csrf_payload.user_id) {
-			(Some(user), Some(user_id)) => {
-				if user.id != user_id {
-					return Err(TransactionError::custom(ApiError::new_const(
-						StatusCode::BAD_REQUEST,
-						"connection already paired with another user",
-					)));
-				}
+		let user_id = match (csrf_payload.user_id, user) {
+			// user tries to link a different account
+			(Some(user_id), Some(user)) if user_id != user.id => {
+				// deny log in
+				return Err(TransactionError::custom(ApiError::new_const(
+					StatusCode::BAD_REQUEST,
+					"connection already paired with another user",
+				)));
 			}
-			(Some(user), None) => {
+			// user links an already linked account
+			// we know that (user_id == user.id) is true here
+			(Some(user_id), Some(_)) => user_id,
+			// user links a new account
+			(Some(user_id), None) => user_id,
+			// user logs in with an existing account
+			(None, Some(user)) => {
 				let connection = user
 					.connections
 					.iter()
-					.find(|c| c.platform == platform && c.platform_id == user_data.id)
-					.ok_or_else(|| {
-						tracing::error!("connection not found");
-						ApiError::INTERNAL_SERVER_ERROR
-					})
-					.map_err(TransactionError::custom)?;
+					.find(|c| c.platform_id == user_data.id)
+					.ok_or(TransactionError::custom(ApiError::INTERNAL_SERVER_ERROR))?;
 
 				if !connection.allow_login {
 					return Err(TransactionError::custom(ApiError::new_const(
@@ -119,10 +110,13 @@ pub async fn handle_callback(global: &Arc<Global>, query: LoginRequest, cookies:
 						"connection is not allowed to login",
 					)));
 				}
+
+				user.id
 			}
+			// user logs in for the first time
 			(None, None) => {
-				// New user creation
-				user = Some(User {
+				// create new user
+				let user = User {
 					connections: vec![UserConnection {
 						platform,
 						platform_id: user_data.id.clone(),
@@ -134,143 +128,73 @@ pub async fn handle_callback(global: &Arc<Global>, query: LoginRequest, cookies:
 						linked_at: chrono::Utc::now(),
 					}],
 					..Default::default()
-				});
+				};
 
-				tx.insert_one::<User>(user.as_ref().unwrap(), None).await?;
+				tx.insert_one::<User>(&user, None).await?;
+
+				user.id
 			}
-			_ => {}
-		}
-
-		let full_user = if let Some(user) = user {
-			// This is correct for users that just got created aswell, as this will simply
-			// load the default entitlements, as the user does not exist yet in the
-			// database.
-			global
-				.user_loader
-				.load_user(global, user)
-				.await
-				.map_err(|_| TransactionError::custom(ApiError::INTERNAL_SERVER_ERROR))?
-		} else if let Some(user_id) = csrf_payload.user_id {
-			global
-				.user_loader
-				.load(global, user_id)
-				.await
-				.map_err(|_| TransactionError::custom(ApiError::INTERNAL_SERVER_ERROR))?
-				.ok_or_else(|| TransactionError::custom(ApiError::new_const(StatusCode::BAD_REQUEST, "user not found")))?
-		} else {
-			unreachable!("user should be created or loaded");
 		};
 
-		if !full_user.has(UserPermission::Login) {
-			return Err(TransactionError::custom(ApiError::new_const(
-				StatusCode::FORBIDDEN,
-				"not allowed to login",
-			)));
-		}
-
-		let logged_connection = full_user
-			.connections
-			.iter()
-			.find(|c| c.platform == platform && c.platform_id == user_data.id);
-
-		if let Some(logged_connection) = logged_connection {
-			if logged_connection.platform_avatar_url != user_data.avatar
-				|| logged_connection.platform_username != user_data.username
-				|| logged_connection.platform_display_name != user_data.display_name
-			{
-				// Update user connection
-				if tx
-					.update_one(
-						filter::filter! {
-							User {
-								#[query(rename = "_id")]
-								id: full_user.user.id,
-								#[query(elem_match)]
-								connections: UserConnection {
-									platform,
-									platform_id: user_data.id,
-								}
-							}
-						},
-						update::update! {
-							#[query(set)]
-							User {
-								#[query(flatten, index = "$")]
-								connections: UserConnection {
-									platform_username: user_data.username,
-									platform_display_name: user_data.display_name,
-									platform_avatar_url: user_data.avatar,
-									updated_at: chrono::Utc::now(),
-								},
-								updated_at: chrono::Utc::now(),
-							}
-						},
-						None,
-					)
-					.await?
-					.matched_count == 0
-				{
-					tracing::error!("failed to update user, no matched count");
-					return Err(TransactionError::custom(ApiError::INTERNAL_SERVER_ERROR));
+		// upsert the connection
+		tx.update_one(
+			filter::filter! {
+				User {
+					#[query(rename = "_id")]
+					id: user_id,
+					#[query(elem_match)]
+					connections: UserConnection {
+						platform,
+						platform_id: &user_data.id,
+					}
 				}
-			}
-		} else {
-			let new_connection = UserConnection {
-				platform,
-				platform_id: user_data.id,
-				platform_username: user_data.username,
-				platform_display_name: user_data.display_name,
-				platform_avatar_url: user_data.avatar,
-				allow_login: true,
-				updated_at: chrono::Utc::now(),
-				linked_at: chrono::Utc::now(),
-			};
+			},
+			update::update! {
+				#[query(set)]
+				User {
+					#[query(flatten, index = "$")]
+					connections: UserConnection {
+						platform_username: user_data.username,
+						platform_display_name: user_data.display_name,
+						platform_avatar_url: user_data.avatar,
+						updated_at: chrono::Utc::now(),
+					},
+					updated_at: chrono::Utc::now(),
+				}
+			},
+			UpdateOptions::builder().upsert(true).build(),
+		)
+		.await?;
 
-			let res = tx
-				.update_one(
-					filter::filter! {
-						User {
-							#[query(rename = "_id")]
-							id: full_user.user.id,
-						}
-					},
-					update::update! {
-						#[query(push)]
-						User {
-							#[query(serde)]
-							connections: &new_connection,
-						},
-						#[query(set)]
-						User {
-							updated_at: chrono::Utc::now(),
-						}
-					},
-					None,
-				)
-				.await?;
-
-			if res.modified_count > 0 {
-				tx.register_event(InternalEvent {
-					actor: Some(full_user.clone()),
-					data: InternalEventData::User {
-						after: full_user.user.clone(),
-						data: InternalEventUserData::AddConnection {
-							connection: new_connection,
-						},
-					},
-					timestamp: chrono::Utc::now(),
-				})?;
-			} else {
-				tracing::error!("failed to insert user connection, no modified count");
-				return Err(TransactionError::custom(ApiError::INTERNAL_SERVER_ERROR));
-			}
+		Ok(user_id)
+	})
+	.await
+	.map_err(|e| match e {
+		TransactionError::Custom(e) => e,
+		e => {
+			tracing::error!(error = %e, "transaction failed");
+			ApiError::INTERNAL_SERVER_ERROR
 		}
+	})?;
 
+	let full_user = global
+		.user_loader
+		.load(global, user_id)
+		.await
+		.ok()
+		.flatten()
+		.ok_or(ApiError::INTERNAL_SERVER_ERROR)?;
+
+	if !full_user.has(UserPermission::Login) {
+		return Err(ApiError::new_const(StatusCode::FORBIDDEN, "not allowed to login"));
+	}
+
+	let res = with_transaction(global, |mut tx| async move {
 		if csrf_payload.user_id.is_none() {
 			let user_session = UserSession {
 				id: Default::default(),
-				user_id: full_user.id,
-				// TODO maybe allow for this to be configurable
+				user_id: user_id,
+				// TODO: maybe allow for this to be configurable
 				expires_at: chrono::Utc::now() + chrono::Duration::days(30),
 				last_used_at: chrono::Utc::now(),
 			};
@@ -278,7 +202,7 @@ pub async fn handle_callback(global: &Arc<Global>, query: LoginRequest, cookies:
 			tx.insert_one::<UserSession>(&user_session, None).await?;
 
 			tx.register_event(InternalEvent {
-				actor: Some(full_user.clone()),
+				actor: Some(full_user),
 				data: InternalEventData::UserSession {
 					after: user_session.clone(),
 					data: StoredEventUserSessionData::Create { platform },
