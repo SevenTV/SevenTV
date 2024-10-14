16ffbba1d226fe05779af324999a08aff6c0fb76
Lennart Kloock <lennart.kloock@protonmail.com>
2024-10-14 21:15:19 +0200

    fix(website): emote cotainer rows

--- apps/website/src/components/layout/emote-container.svelte
+++ apps/website/src/components/layout/emote-container.svelte
@@ -18,6 +18,7 @@
 	.emotes {
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
+		grid-template-rows: repeat(auto-fill, 10rem);
 		place-items: center;
 		gap: 1rem;
 
@@ -29,6 +30,7 @@
 
 		&.small-grid {
 			grid-template-columns: repeat(auto-fill, minmax(5rem, 1fr));
+			grid-template-rows: repeat(auto-fill, 5rem);
 			gap: 0.5rem;
 		}
 	}
