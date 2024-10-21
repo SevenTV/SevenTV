5c900d3f4916669da7e218f084a99e904df18b7c
Lennart Kloock <lennart.kloock@protonmail.com>
2024-10-21 21:27:09 +0200

    add basic code

--- apps/reprocess-badges/src/main.rs
+++ apps/reprocess-badges/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-	println!("Hello, world!");
+use futures::TryStreamExt;
+use shared::{
+	config::DatabaseConfig,
+	database::{badge::Badge, queries::filter, MongoCollection},
+};
+
+#[tokio::main]
+async fn main() {
+	let config = DatabaseConfig { uri: "".to_string() };
+
+	let mongo = shared::database::setup_database(&config, false).await.unwrap();
+	let db = mongo.default_database().unwrap();
+
+	let mut badges = Badge::collection(&db)
+		.find(filter::filter! {
+			Badge {}
+		})
+		.await
+		.unwrap();
+
+	while let Some(badge) = badges.try_next().await.unwrap() {
+		println!("{:?}", badge);
+	}
 }
