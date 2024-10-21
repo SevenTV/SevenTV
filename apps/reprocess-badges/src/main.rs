703cdff7673aa9a5d217a85b419282957c416289
Lennart Kloock <lennart.kloock@protonmail.com>
2024-10-21 22:49:29 +0200

    feat: add reprocessing code

--- apps/reprocess-badges/src/main.rs
+++ apps/reprocess-badges/src/main.rs
@@ -2,7 +2,16 @@ use scuffle_foundations::{
 	bootstrap::{bootstrap, Bootstrap},
 	settings::{auto_settings, cli::Matches},
 };
-use shared::config::DatabaseConfig;
+use shared::{
+	config::{DatabaseConfig, ImageProcessorConfig},
+	database::{
+		badge::Badge,
+		image_set::{ImageSet, ImageSetInput},
+		queries::{filter, update},
+		MongoCollection,
+	},
+	image_processor::ImageProcessor,
+};
 
 mod badges;
 
@@ -10,6 +19,7 @@ mod badges;
 #[serde(default)]
 struct Config {
 	database: DatabaseConfig,
+	image_processor: ImageProcessorConfig,
 }
 
 impl Bootstrap for Config {
@@ -18,15 +28,71 @@ impl Bootstrap for Config {
 
 #[bootstrap]
 async fn main(settings: Matches<Config>) {
-	let config = DatabaseConfig {
-		uri: settings.settings.database.uri.clone(),
-	};
-
-	let mongo = shared::database::setup_database(&config, false).await.unwrap();
+	let mongo = shared::database::setup_database(&settings.settings.database, false)
+		.await
+		.unwrap();
 	let db = mongo.default_database().unwrap();
 
-	for badge in badges::jobs() {
-		println!("{:?}", badge);
+	let ip = ImageProcessor::new(&settings.settings.image_processor)
+		.await
+		.expect("failed to initialize image processor");
+
+	for job in badges::jobs() {
+		tracing::info!("reprocessing {:?}", job.input);
+
+		let data = tokio::fs::read(job.input).await.expect("failed to read input file");
+
+		match ip.upload_badge(job.id, data.into()).await {
+			Ok(scuffle_image_processor_proto::ProcessImageResponse {
+				id,
+				upload_info:
+					Some(scuffle_image_processor_proto::ProcessImageResponseUploadInfo {
+						path: Some(path),
+						content_type,
+						size,
+					}),
+				error: None,
+			}) => {
+				let image_set = ImageSet {
+					input: ImageSetInput::Pending {
+						task_id: id,
+						path: path.path,
+						mime: content_type,
+						size: size as i64,
+					},
+					outputs: vec![],
+				};
+
+				Badge::collection(&db)
+					.update_one(
+						filter::filter! {
+							Badge {
+								#[query(rename = "_id")]
+								id: job.id,
+							}
+						},
+						update::update! {
+							#[query(set)]
+							Badge {
+								#[query(serde)]
+								image_set,
+								updated_at: chrono::Utc::now(),
+								search_updated_at: &None,
+							}
+						},
+					)
+					.await
+					.expect("failed to clear image set");
+			}
+			Ok(res) => {
+				tracing::error!(res = ?res, "invalid image processor response");
+				continue;
+			}
+			Err(e) => {
+				tracing::error!(error = ?e, "failed to start send image processor request");
+				continue;
+			}
+		}
 	}
 
 	std::process::exit(0);
