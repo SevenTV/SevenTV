8188729eff77b31042e1915736d40a7912b35ded
Lennart Kloock <lennart.kloock@protonmail.com>
2024-12-08 15:12:44 +0100

    fix(website): weird js error

--- apps/website/src/lib/gql.ts
+++ apps/website/src/lib/gql.ts
@@ -5,8 +5,12 @@ import { sessionToken } from "$/lib/auth";
 import { PUBLIC_GQL_API_V4 } from "$env/static/public";
 import { currentError, errorDialogMode } from "./error";
 
+// var is used on purpose here to prevent a weird error when calling the function below
+// "Uncaught (in promise) ReferenceError: can't access lexical declaration 'client' before initialization"
+
 // non-reactive on purpose
-let client: Client | undefined = undefined;
+// eslint-disable-next-line no-var
+var client: Client | undefined;
 
 export function gqlClient(): Client {
 	if (client) return client;
