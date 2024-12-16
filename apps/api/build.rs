d66f32a3a66951f2a92fbb86553480b2eb82e55f
Lennart Kloock <lennart.kloock@protonmail.com>
2024-12-16 18:12:48 +0100

    chore(api): clippy

--- apps/api/build.rs
+++ apps/api/build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
 	// Include the current commit hash
 	if let Some(hash) = Command::new("git")
-		.args(&["rev-parse", "HEAD"])
+		.args(["rev-parse", "HEAD"])
 		.output()
 		.ok()
 		.and_then(|o| String::from_utf8(o.stdout).ok())
