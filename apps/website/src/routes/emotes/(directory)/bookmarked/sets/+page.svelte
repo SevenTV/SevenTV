59972a81d8fb6ea74088eb9003c8ef15dfea9a14
Lennart Kloock <lennart.kloock@protonmail.com>
2024-12-23 15:15:23 +0100

    refactor(website): remove unused pages

--- apps/website/src/routes/emotes/(directory)/bookmarked/sets/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { t } from "svelte-i18n";
-</script>
-
-<svelte:head>
-	<title>{$t("page_titles.bookmarked_emote_sets")} - {$t("page_titles.suffix")}</title>
-</svelte:head>
