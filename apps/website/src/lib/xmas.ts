3eb23000bc0c796bc2431e986985d4d839438d09
Matin Aniss <76515905+MatinAniss@users.noreply.github.com>
2024-12-30 11:49:12 +1100

    fix(website): event end date

--- apps/website/src/lib/xmas.ts
+++ apps/website/src/lib/xmas.ts
@@ -3,6 +3,6 @@ import moment from "moment";
 export function isXmasEvent() {
 	return (
 		moment().isAfter(moment("2024-12-14T00:00:00Z")) &&
-		moment().isBefore(moment("2024-12-27T00:00:00Z"))
+		moment().isBefore(moment("2024-12-31T00:00:00Z"))
 	);
 }
