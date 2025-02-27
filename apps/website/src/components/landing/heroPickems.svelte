4d01a712987bf85dc9248e04d8595b846bc174cc
mos377 <mark@7tv.app>
2025-02-27 14:18:58 +0000

    minor text change to reflect correct number of streamers on the front page

--- apps/website/src/components/landing/heroPickems.svelte
+++ apps/website/src/components/landing/heroPickems.svelte
@@ -72,7 +72,7 @@
 		{/if}
 		<div class="hero-content centered">
 			<div class="top-info hide-on-mobile">
-				<p>16 STREAMERS</p>
+				<p>17 STREAMERS</p>
 				<Minus />
 				<p>7TV HOSTED TOURNAMENT</p>
 				<Minus />
@@ -84,7 +84,7 @@
 					<h1>Win Badges & Paints.</h1>
 				</div>
 				<div class="description">
-					<p>xQc, OhnePixel, JasonTheWeen and 13 other streamers go head to head</p>
+					<p>xQc, OhnePixel, JasonTheWeen and 14 other streamers go head to head</p>
 					<p>in a 3-day Single Elimination 7TV Hosted CS2 tournament.</p>
 				</div>
 			</div>
