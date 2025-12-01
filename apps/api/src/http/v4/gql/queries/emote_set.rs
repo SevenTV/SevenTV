b597096ce5ef80ec12e2ea5dd991143d9a3c0921
creepycode <42820438+ByteZ1337@users.noreply.github.com>
2025-12-01 14:23:02 +0100

    feat: new v4 gql qol queries (#271)
    * add badge by id v4 gql query
    
    * add paint by id v4 gql query
    
    * add User.createdAt field to v4 gql
    
    * add global emote set v4 gql query
    
    * move created_at into a function for consistency

--- apps/api/src/http/v4/gql/queries/emote_set.rs
+++ apps/api/src/http/v4/gql/queries/emote_set.rs
@@ -45,4 +45,30 @@ impl EmoteSetQuery {
 
 		Ok(ids.iter().filter_map(|id| emote_sets.remove(id)).map(Into::into).collect())
 	}
+
+	#[tracing::instrument(skip_all, name = "EmoteSetQuery::global")]
+	async fn global(
+		&self,
+		ctx: &Context<'_>,
+	) -> Result<EmoteSet, ApiError> {
+		let global: &Arc<Global> = ctx
+			.data()
+			.map_err(|_| ApiError::internal_server_error(ApiErrorCode::MissingContext, "missing global data"))?;
+
+		let config = global
+			.global_config_loader
+			.load(())
+			.await
+			.map_err(|_| ApiError::internal_server_error(ApiErrorCode::LoadError, "failed to load global config"))?
+			.ok_or_else(|| ApiError::not_found(ApiErrorCode::LoadError, "global config not found"))?;
+
+		let emote_set = global
+			.emote_set_by_id_loader
+			.load(config.emote_set_id)
+			.await
+			.map_err(|()| ApiError::internal_server_error(ApiErrorCode::LoadError, "failed to load global emote set"))?
+			.ok_or_else(|| ApiError::not_found(ApiErrorCode::LoadError, "global emote set not found"))?;
+
+		Ok(emote_set.into())
+	}
 }
