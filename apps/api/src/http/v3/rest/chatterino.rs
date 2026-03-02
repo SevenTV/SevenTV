cd3d3b183caff640f2f3c41041794d5c71bc2d5d
Nerixyz <nerixdev@outlook.de>
2026-03-02 10:30:40 +0100

    refactor: Handle 7.x to 2.x change and WOA (#279)

--- apps/api/src/http/v3/rest/chatterino.rs
+++ apps/api/src/http/v3/rest/chatterino.rs
@@ -44,7 +44,9 @@ async fn get_version_info(
 	macro_rules! make_statics {
 		() => {{
 			static VALUE: OnceLock<serde_json::Value> = OnceLock::new();
-			Ok(Json(VALUE.get_or_init(|| os.info_for_version(branch.resolve(&global)))))
+			Ok(Json(
+				VALUE.get_or_init(|| os.info_for_version(&global, branch.resolve(&global))),
+			))
 		}};
 	}
 
@@ -66,28 +68,52 @@ enum Os {
 }
 
 impl Os {
-	pub fn info_for_version(self, version: &str) -> serde_json::Value {
+	pub fn info_for_version(self, global: &Global, version: &str) -> serde_json::Value {
+		// For backwards compatiblity:
+		// Chatterino 7TV versions before 7.5.5 did not consider 7.x.y
+		// to 2.x.(y+1) as an update, so we pretend the new version is actually
+		// 7.x.(y+1).
+		// For the newer versions, we include `v2_version` which contains the
+		// actual version.
+		let v7_version = if version.starts_with("2.") {
+			Some(version.replacen("2.", "7.", 1))
+		} else {
+			None
+		};
+		let v7_version = v7_version.as_deref().unwrap_or(version);
+
+		let experimental = if global.config.api.chatterino.windows_on_arm_is_experimental {
+			"Experimental-"
+		} else {
+			""
+		};
+
+		let win_installer_x86 =
+			format!("https://github.com/SevenTV/chatterino7/releases/download/v{version}/Chatterino7.Installer.zip");
+		let win_portable_x86 =
+			format!("https://github.com/SevenTV/chatterino7/releases/download/v{version}/Chatterino7.Portable.zip");
+		let win_installer_arm = format!("https://github.com/SevenTV/chatterino7/releases/download/v{version}/{experimental}ARM64-Chatterino7.Installer.zip");
+		let win_portable_arm = format!("https://github.com/SevenTV/chatterino7/releases/download/v{version}/{experimental}ARM64-Chatterino7.Portable.zip");
 		match self {
 			Os::Windows => serde_json::json!({
-				"version": version,
-				"updateexe": format!(
-					"https://github.com/SevenTV/chatterino7/releases/download/v{}/Chatterino7.Installer.exe", version
-				),
-				"portable_download": format!(
-					"https://github.com/SevenTV/chatterino7/releases/download/v{}/Chatterino7.Portable.zip", version
-				),
+				"version": v7_version,
+				"v2_version": version,
+				"updateexe": win_installer_x86,
+				"updateexe_x86": win_installer_x86,
+				"updateexe_arm": win_installer_arm,
+				"portable_download": win_portable_x86,
+				"portable_download_x86": win_portable_x86,
+				"portable_download_arm": win_portable_arm,
 			}),
 			Os::MacOs => serde_json::json!({
-				"version": version,
-				"updateexe": format!(
-					"https://github.com/SevenTV/chatterino7/releases/download/v{}/Chatterino.dmg", version
-				),
+				"version": v7_version,
+				"v2_version": version,
+				"updateexe": format!("https://github.com/SevenTV/chatterino7/releases/download/v{version}/Chatterino.dmg"),
 			}),
 			Os::Linux => serde_json::json!({
-				"version": version,
-				"updateguide": format!(
-					"https://github.com/SevenTV/chatterino7/releases/tag/v{}", version
-				),
+				"version": v7_version,
+				"v2_version": version,
+				"updateguide": format!("https://github.com/SevenTV/chatterino7/releases/tag/v{version}"),
 			}),
 		}
 	}
